17eaad81b0158a6d10208f24c8d99c022d486cc2
Damien Leroy <damien7leroy@gmail.com>
2015-12-15 13:56:11 +0100

    Restructuration de la classe Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,24 +9,77 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Client {
-    public static void main(String[] zero) {
-        Socket socket;  //Création d'une variable socket
-		BufferedReader in;  // Flux entrant
-        PrintWriter out;    // Flux sortant
-        try {
-            socket = new Socket("192.168.0.94",2010);  //Envoi d'un socket à l'adresse LocalHost sur le port 2009
-			in = new BufferedReader (new InputStreamReader (socket.getInputStream()));  // Déclaration du flux entrant
-            out = new PrintWriter(socket.getOutputStream());    // Déclaration du flux sortant
-            out.println("Message pour le serveur"); // Envoie d'un message au serveur
-            out.flush();
-            socket.close(); //Fermeture du socket
-        }catch (UnknownHostException e) //Si le serveur n'est pas trouvé
-        {
-            e.printStackTrace();
-        }catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-    }
+public class Client
+{
+	private Socket socket; // Création d'une variable socket
+	private BufferedReader in; // Flux entrant
+	private PrintWriter out;   // Flux sortant
+	private String adresseServeur;  // Adresse IPv4 du serveur
+	private int port;   // Port de connexion au serveur
+
+	public Client(String adresseServeur, int port)
+	{
+		this.adresseServeur = adresseServeur;
+		this.port = port;
+		try
+		{
+			socket = new Socket(adresseServeur, port);   // Ouverture de la socket
+		}
+		catch (IOException e) //Si le serveur n'est pas trouvé
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public boolean envoiMessage(String message)
+	{
+		try
+		{
+			out = new PrintWriter(this.socket.getOutputStream());    // Déclaration du flux sortant
+			out.println(message); // Envoie d'un message au serveur
+			out.flush();
+		}
+		catch (IOException e)   //En cas d'erreur
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public String lectureMessage()
+	{
+		try
+		{
+			in = new BufferedReader(new InputStreamReader (this.socket.getInputStream())); //permet de lire les caractères provenant du socket
+			return in.readLine();   //Renvoie le contenu de in
+		}
+		catch (IOException e)   //En cas d'erreur
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void fermetureClient()
+	{
+		try
+		{
+			socket.close();   //Ferme le socket
+		}
+		catch (IOException ex)  //En cas d'erreur
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Client c = new Client("192.168.0.94", 2010);
+		if (c.envoiMessage("Bonjour, je suis un client."))
+			System.out.println("Message du serveur : " + c.lectureMessage());
+		else
+			System.out.println("Erreur lors de l'envoie du message");
+		c.fermetureClient();
+	}
 }
