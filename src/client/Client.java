0b421ea05c562645ee40bb95f20401b68b1af99c
Damien Leroy <damien7leroy@gmail.com>
2015-12-15 11:33:32 +0100

    Envoie d'un message au serveur par socket

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,15 +1,25 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] zero) {
         Socket socket;  //Création d'une variable socket
+		BufferedReader in;  // Flux entrant
+        PrintWriter out;    // Flux sortant
         try {
-            socket = new Socket(InetAddress.getLocalHost(),2009);  //Envoi d'un socket à l'adresse LocalHost sur le port 2009
+            socket = new Socket("192.168.0.94",2010);  //Envoi d'un socket à l'adresse LocalHost sur le port 2009
+			in = new BufferedReader (new InputStreamReader (socket.getInputStream()));  // Déclaration du flux entrant
+            out = new PrintWriter(socket.getOutputStream());    // Déclaration du flux sortant
+            out.println("Message pour le serveur"); // Envoie d'un message au serveur
+            out.flush();
             socket.close(); //Fermeture du socket
         }catch (UnknownHostException e) //Si le serveur n'est pas trouvé
         {
