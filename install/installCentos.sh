397f490994be679f8f00829b3390bc5ff0089485
Damien Leroy <damien7leroy@gmail.com>
2016-02-05 15:18:36 +0100

    Automatisation des installations

--- install/installCentos.sh
+++ install/installCentos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install git
-yum install java-devel
+yum -y install git
+yum -y install java-devel
 git clone https://damleroy@bitbucket.org/damleroy/clientfork.git
 echo "You need to configure the client now"
